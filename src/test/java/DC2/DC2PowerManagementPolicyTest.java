c3823a5209c7e3520e76dbddd8605b5a99dc29ea
lyzl <lyzl5764@hotmail.com>
2019-03-22 21:54:54 +0000

    Implemented two unit tests

---
+++ src/test/java/DC2/DC2PowerManagementPolicyTest.java
@@ -0,0 +1,20 @@
+package DC2;
+import org.junit.Test;
+
+
+public class DC2PowerManagementPolicyTest {
+    @Test
+    public void optimalCpuTest() {
+        int i=-1;
+        DC2PowerManagementPolicy pmc = new DC2PowerManagementPolicy();
+        pmc.checkOptimalCPUnumber(i);
+
+    }
+    @Test
+    public void checkNegativeCpuTest() {
+        int i=1;
+        DC2PowerManagementPolicy pmc = new DC2PowerManagementPolicy();
+        pmc.checkOptimalCPUnumber(i);
+
+    }
+}
--- src/test/java/DC2/DC2PredictionTest.java
+++ src/test/java/DC2/DC2PredictionTest.java
@@ -1,28 +1,57 @@
 package DC2;
 
+//import DC2.*;
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.junit.Before;
 
 import static org.junit.Assert.*;
 
 public class DC2PredictionTest {
-
-    DC2Prediction prediction = new DC2Prediction(10L);
-
-    int cpuOccupation = 100;
-
-    float optimalRate = 0.6f;
-
-    @Test
-    public void optimalRateTest(){
-        assertEquals(optimalRate, prediction.getOptimalRate(), 0.0002);
+    private DC2Prediction prediction;
+    private DC2Database database;
+    Queue<Float> occupationHistoryQueue = new LinkedList<Float>();
+    DC2CpuAgent dc2CpuAgent = new DC2CpuAgent("1023",90,30l);
+    DC2CpuRecordingMatrix dcasa = new DC2CpuRecordingMatrix(dc2CpuAgent);
+    ArrayList<DC2CpuRecordingMatrix> recordingList = new ArrayList<>();
+    DC2Prediction dc2Prediction = new DC2Prediction(10l);
+    boolean thrown;
+    @Before
+    public void before()
+    {
+        occupationHistoryQueue.add(0.0f);
+        recordingList.add(dcasa);
+        thrown = false;
     }
-
     @Test
-    public void totalCPUOccupationTest(){
-
-        prediction.mainTask();
-        assertTrue(prediction.getTotalCpuOccupation() <= optimalRate);
-        assertTrue(prediction.getTotalCpuOccupation() >= 0);
+    public  void checkTotalOccupation(){
+        occupationHistoryQueue.add(0.0f);
+        try{  dc2Prediction.predict(occupationHistoryQueue,recordingList);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
     }
-
+    @Test
+    public void checkTotalOccupation1(){
+        occupationHistoryQueue.add(101.0f);
+        try{
+            dc2Prediction.predict(occupationHistoryQueue,recordingList);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+    @Test
+    public void checkpuCount(){
+        occupationHistoryQueue.add(-2.0f);
+        try{  dc2Prediction.predict(occupationHistoryQueue,recordingList);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
 }
