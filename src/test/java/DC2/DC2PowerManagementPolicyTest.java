babccf63e698f0d4a09969f5047b01d23746ef9a
lyzl <lyzl5764@hotmail.com>
2019-04-05 20:51:52 +0100

    Fix the bug cause wrong boundary in unit test

--- src/test/java/DC2/DC2PowerManagementPolicyTest.java
+++ src/test/java/DC2/DC2PowerManagementPolicyTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 public class DC2PowerManagementPolicyTest {
     @Test
     public void optimalCpuTest() {
-        int i=-1;
+        int i=1;
         DC2PowerManagementPolicy pmc = new DC2PowerManagementPolicy();
         pmc.checkOptimalCPUnumber(i);
 
     }
     @Test
     public void checkNegativeCpuTest() {
-        int i=1;
+        int i=-1;
         DC2PowerManagementPolicy pmc = new DC2PowerManagementPolicy();
         pmc.checkOptimalCPUnumber(i);
 
